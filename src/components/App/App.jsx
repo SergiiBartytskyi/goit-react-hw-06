328ee0c83b1764ec3baef63bb8f62b9a2695691c
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2024-09-09 14:14:46 +0200

    refactoring task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,19 @@
-import css from "./App.module.css";
+import { Layout } from "../Layout/Layout";
+import ContactForm from "../ContactForm/ContactForm";
+import SearchBox from "../SearchBox/SearchBox";
+import ContactList from "../ContactList/ContactList";
 
-function App() {
-  return <></>;
-}
+// import css from "./App.module.css";
+
+const App = () => {
+  return (
+    <Layout>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </Layout>
+  );
+};
 
 export default App;
